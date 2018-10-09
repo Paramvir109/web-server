6a99e917da078ce37a9029ff36053ee9bef76e5d
Paramvir Singh <paramvir109@gmail.com>
2018-10-09 18:17:37 +0530

    Added projects feature

--- server.js
+++ server.js
@@ -56,6 +56,12 @@ app.get('/bad', (req, res) => {
     });
 });
 
+app.get('/projects' , (req, res)=> {
+    res.render('projects.hbs', {
+        pageTitle: 'My Projects'
+    })
+});
+
 app.listen(port ,() => {
     console.log(`Server is up and running on ${port} port`);
 });//Never stops actually
